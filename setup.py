d43d3b46507ed76dbc7faf200758abaf856ad7f0
donsheehy <don.r.sheehy@gmail.com>
2021-03-15 11:52:37 -0400

    Version bump to 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ds2viz",
-    version="0.1.3",
+    version="0.1.4",
     author="Donald R. Sheehy",
     author_email="don.r.sheehy@gmail.com",
     description="Don Sheehy's Data Structures Visualizations",
