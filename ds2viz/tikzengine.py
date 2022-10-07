0d2307b3f84c240886f8890526a775a11bc4cf55
Oliver Chubet <oliver.chubet@gmail.com>
2022-10-07 11:44:46 -0400

    tikzengine passes 'eyetest'

--- ds2viz/tikzengine.py
+++ ds2viz/tikzengine.py
@@ -1,11 +1,31 @@
 from ds2viz.primitives import *
 from ds2viz.imageengine import ImageEngine, styledefaults
 
-def rgbtohex(rgb):
+scale = .02 
+
+def rgbtotikz(rgb):
     if rgb is None:
         return 'none'
-    colors = [hex(int(x * 255 + 256))[-2:] for x in rgb]
-    return "#" + "".join(colors[:3])
+    else:
+        red,green,blue = rgb
+#        if red == 1 and green == 1 and blue == 1:
+#            return 'white'
+#        else:
+        return f'{{rgb,1:red,{red};green,{green};blue,{blue}}}'
+
+def tikzpt(pt):
+        x,y = pt
+        return f'({x*scale:.2f}, {-y*scale:.2f})'
+
+def tikzprops(props):
+    tpropslist = []
+    if 'fill' in props.keys():
+        tpropslist.append(f'fill={props["fill"]}')
+    if 'stroke' in props.keys():
+        tpropslist.append(f'stroke={props["stroke"]}')
+    if 'stroke_width' in props.keys():
+        tpropslist.append(f'line width={props["stroke_width"]/2:.2f}')
+    return ", ".join(tpropslist)
 
 class TikzEngine(ImageEngine):
     def __init__(self, canvas, filename = None):
@@ -18,41 +38,43 @@ class TikzEngine(ImageEngine):
             if prop not in props:
                 props[prop] = styledefaults[prop]
             if prop in ['stroke', 'fill']:
-                output[prop] = rgbtohex(props[prop])
+                output[prop] = rgbtotikz(props[prop])
             else:
                 output[prop] = props[prop]
         return output
 
     def draw_circle(self, circle):
         props = self.props(circle.props, 'stroke', 'fill', 'stroke_width')
-        center = list(circle.center)
-        radius = circle.radius
-        tcircle = f'\\draw {center} circle {radius};\n'
-        self.tikz.append(tcircle)
+        tprops = tikzprops(props)
+        center = tikzpt(circle.center)
+        radius = circle.radius * scale
+        tcircle = f'\\draw[{tprops}] {center} circle ({radius:.2f});\n'
+        self.tikz += tcircle
 
     def draw_polyline(self, polyline):
         props = self.props(polyline.props, 'stroke', 'fill', 'stroke_width')
-        points = list(list(p) for p in polyline.points)
-        tpolyline = '\\draw' + ' -- '.join(points) + ';\n'
-        self.tikz.append(tpolyline)
+        tprops = tikzprops(props)
+        points = list(tikzpt(p) for p in polyline.points)
+        tpolyline = f'\\draw[{tprops}] ' + ' -- '.join(points) + ';\n'
+        self.tikz += tpolyline
 
     def draw_polygon(self, polygon):
         props = self.props(polygon.props, 'stroke', 'fill', 'stroke_width')
-        fill = props['fill']
-        points = list(list(p) for p in polygon.points)
-        tpolyline = '\\filldraw[{fill}]' + ' -- '.join(points) + ';\n'
-        self.tikz.append(tpolyline)
+        tprops = tikzprops(props)
+        points = list(tikzpt(p) for p in polygon.points)
+        points.append(points[0])
+        tpolyline = f'\\draw[{tprops}]' + ' -- '.join(points) + ';\n'
+        self.tikz +=  tpolyline
 
     def draw_bezier(self, bezier):
         props = self.props(bezier.props, 'stroke', 'fill', 'stroke_width')
-        # points = list(tuple(p) for p in bezier.points)
-        points = ['('+x+','+y+')' for x,y in bezier.points
-        controls = points[1:-2]
+        tprops = tikzprops(props)
+        points = [tikzpt(p) for p in bezier.points]
+        controls = points[1:-1]
         first = points[0]
         last = points[-1]
-        
-        tbezier = '\\draw {first} .. controls' + ' and '.join(controls) + '.. {str(last)};\n'
-        self.tikz.append(tbezier)
+        tbezier = f'\\draw[{tprops}] {first} .. controls ' + ' and '.join(controls) + f' .. {str(last)};\n'
+        self.tikz += tbezier
 
     def draw_text(self, text):
         props = self.props(text.props,
@@ -64,10 +86,11 @@ class TikzEngine(ImageEngine):
                            # 'dominant_baseline',
                            # 'text_anchor',
                            )
+        tprops = tikzprops(props)
         string = text.text
-        print(string)
-        ttext = f'\node[draw] at {text.position} {text.text};\n'
-        selk.tikz.append(ttext)
+        position = tikzpt(text.position)
+        ttext = f'\\draw[{tprops}] {position} node {{{text.text}}};\n'
+        self.tikz += ttext
 
     def __str__(self):
-        return self.tikz
+        return '\\begin{tikzpicture}\n' + self.tikz + '\\end{tikzpicture}'
